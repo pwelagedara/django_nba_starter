03bf6062b5912e4c1369a52f7ea4101fd8fcce1a
Pubudu Welagedara <pubuduwelagedara@gmail.com>
2021-01-25 13:43:06 +0530

    trying to fix issue

--- django_nba_starter/settings.py
+++ django_nba_starter/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
 AUTH_USER_MODEL = 'api_services.User'
