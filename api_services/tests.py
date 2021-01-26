8f522525df049736c161600b96075597c0cdaefe
Pubudu Welagedara <pubuduwelagedara@gmail.com>
2021-01-26 23:26:02 +0530

    testing code coverage

--- api_services/tests.py
+++ api_services/tests.py
@@ -1,18 +1,33 @@
 import json
+import random
 
-from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from django.core import management
 
-from api_services import models, serializers
+from api_services import models, serializers, enums
 
 
 class LoginTestCase(APITestCase):
 
     def setUp(self):
         management.call_command('initializedata')
+
+        admin = list(models.User.objects.filter(role=enums.RoleChoice.ADMIN))[0]
+
+        coaches = list(models.User.objects.filter(role=enums.RoleChoice.COACH))
+        random.shuffle(coaches)
+        coach = coaches.pop()
+
+        players = list(models.User.objects.filter(role=enums.RoleChoice.PLAYER))
+        random.shuffle(players)
+        player = players.pop()
+
+        self.admin_token = Token.objects.create(user=admin)
+        self.coach_token = Token.objects.create(user=coach)
+        self.player_token = Token.objects.create(user=player)
 
     def test_login_success(self):
         data = {
--- django_nba_starter/settings.py
+++ django_nba_starter/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
